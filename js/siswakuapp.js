adcff8eaf99ca9caf25a0f6384b0abe19808d1c2
MrRangga <40726290+MrRangga@users.noreply.github.com>
2020-03-20 06:18:18 +0700

    init the project

---
+++ js/siswakuapp.js
@@ -0,0 +1,10 @@
+
+    $(document).ready(function(){
+
+        // Department Change
+            $('#nama_hari').change('click',function(){
+                var nama_hari = $('.nama_hari').val();
+                console.log(nama_hari);  
+            });
+
+        });  
